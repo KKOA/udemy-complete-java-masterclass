6f6e348edc92bc84801b2ccf28cf6271f5ccac34
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-05 07:04:49 +0100

    Updated references in ch08.exercises MyMain class

--- udemy-complete-java-masterclass/src/ch08/challenges/MyMain.java
+++ udemy-complete-java-masterclass/src/ch08/challenges/MyMain.java
@@ -6,6 +6,24 @@ public class MyMain {
 		System.out.println("Chaper 8 Challenges");
 		System.out.println();
 
+		// Bedroom
+		Wall wall1 = new Wall("West");
+		Wall wall2 = new Wall("North");
+		Wall wall3 = new Wall("East");
+		Wall wall4 = new Wall("South");
+
+		Ceiling ceiling = new Ceiling(12, 55);
+
+		Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+		Lamp lamp = new Lamp("Classic", false, 75);
+
+		Bedroom bedroom = new Bedroom("Keith", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+		bedroom.makeBed();
+		bedroom.getLamp().turnOn();
+
+		// Printer
+		System.out.println();
 		System.out.println("Printer 1");
 		Printer printer = new Printer(50, true);
 		System.out.println("Intial page count = " + printer.getPagesPrinted());
--- udemy-complete-java-masterclass/src/ch08/exercises/GoodPlayer.java
+++ udemy-complete-java-masterclass/src/ch08/exercises/GoodPlayer.java
@@ -25,6 +25,7 @@ public class GoodPlayer {
 
 	}
 
+	// Getter
 	public int HealthRemaining() {
 		return hp;
 	}
