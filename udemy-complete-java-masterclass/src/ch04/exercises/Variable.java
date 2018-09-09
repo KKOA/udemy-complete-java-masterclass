c702e96d47f6c792eb65224b70584abc27d2f5e4
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-09 20:10:05 +0100

    Added variable class

---
+++ udemy-complete-java-masterclass/src/ch04/exercises/Variable.java
@@ -0,0 +1,39 @@
+package ch04.exercises;
+
+/**
+ * Created 8th Aug 2015
+ * 
+ * @author Keith
+ *
+ */
+public class Variable {
+
+	public static void main() {
+
+		// Declare and initialise variable
+		int myFirstNumber = 5;
+		// data-type variable-name = value
+
+		// Reassign variable to result calculation
+		myFirstNumber = 10 + 5;
+
+		System.out.println(myFirstNumber);
+		System.out.println("myFirstNumber");
+		// Print out string instead of variable
+
+		// Declare and initialise multiple variables
+		int mySecondNumber = 12, myThirdNumber = 6;
+
+		// Declare and initialise variable with value dependent on variable
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+		System.out.println(myTotal);
+
+		int newmyTotal = 1000 - myTotal;
+		// variable names are case sensitive
+
+		System.out.println(newmyTotal);
+
+	}
+
+}
