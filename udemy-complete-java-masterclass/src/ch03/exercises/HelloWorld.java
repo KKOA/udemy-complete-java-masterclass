331d73f08ac5595f16308eb8c9c66b6d09c076c6
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-09 20:03:10 +0100

    Moved print statements from HelloWorld to MyMain in ch03.exercises

--- udemy-complete-java-masterclass/src/ch03/exercises/HelloWorld.java
+++ udemy-complete-java-masterclass/src/ch03/exercises/HelloWorld.java
@@ -2,18 +2,11 @@ package ch03.exercises;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main() {
 		// accessor [class-method] return-type method-name (data-type parameter)
-
-		System.out.println("Chapter 3 Exercises");
-		System.out.println();
 
 		// Output value to console
 		System.out.println("Hello World");
-
-		System.out.println();
-		System.out.println("End of Chapter 3 Exercises");
-		System.out.println();
 
 	}
 
---
+++ udemy-complete-java-masterclass/src/ch03/exercises/MyMain.java
@@ -0,0 +1,12 @@
+package ch03.exercises;
+
+public class MyMain {
+	public static void main() {
+		System.out.println("Chapter 3 Exercises");
+		System.out.println();
+		HelloWorld.main();
+		System.out.println();
+		System.out.println("End of Chapter 3 Exercises");
+		System.out.println();
+	}
+}
