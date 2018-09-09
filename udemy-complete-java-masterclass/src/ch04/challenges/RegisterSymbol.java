947f41e1c24fb65e0a18bf7082eac1fad27a445a
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-09 20:16:39 +0100

    Added RegisterSymbol Class

---
+++ udemy-complete-java-masterclass/src/ch04/challenges/RegisterSymbol.java
@@ -0,0 +1,12 @@
+package ch04.challenges;
+
+public class RegisterSymbol {
+
+	public static void main() {
+		char registerSign = '\u00AE';
+//		System.out.println(registerSign);
+		System.out.println("Unicode ouput : " + registerSign);
+
+	}
+
+}
