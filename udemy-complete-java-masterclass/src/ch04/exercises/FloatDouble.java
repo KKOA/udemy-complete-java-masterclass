0fa21c0209b079f4386dbbfb3c80e445bd341da3
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-09 20:13:14 +0100

    Added FloatDouble class

---
+++ udemy-complete-java-masterclass/src/ch04/exercises/FloatDouble.java
@@ -0,0 +1,31 @@
+package ch04.exercises;
+
+public class FloatDouble {
+
+	public static void main() {
+
+		int myIntValue = 5;
+		int myNewIntValue = myIntValue / 2;
+
+		float myFloatValue = (float) 5;
+		// Java by default use double for floating point numbers
+
+		double myDoubleValue = 5d;
+		// d at the end number is optional when using float numbers as it already cast
+		// to double
+
+		System.out.println("My Float value = " + myIntValue);
+		System.out.println("My Float value = " + myFloatValue);
+		System.out.println("My double value = " + myDoubleValue);
+		System.out.println("Integer division = " + myIntValue + " / " + 2 + " = " + myNewIntValue);
+		// Decimal number dropped 5/2 = 2;
+		System.out.println("Double division = " + myFloatValue + " / " + 2f + " = " + (myFloatValue / 2f));
+		// Add f end of the number tell java that this a float not a double
+
+		// Double over float
+		// more precise
+		// faster and default return type for many of java built in functions
+
+	}
+
+}
