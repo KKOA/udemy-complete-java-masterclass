8f7644a17efd56ac7c5dd94f2d607afc5f459ff0
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-09 20:52:32 +0100

    Added MyMain Class to package ch04.challenges. Added references to all classes in the same package.

---
+++ udemy-complete-java-masterclass/src/ch04/challenges/MyMain.java
@@ -0,0 +1,15 @@
+package ch04.challenges;
+
+public class MyMain {
+	public static void main() {
+		System.out.println("Chapter 4 Challenges");
+		System.out.println();
+		ByteShortInt.main();
+		PoundsToKilograms.main();
+		RegisterSymbol.main();
+		Operator.main();
+		System.out.println();
+		System.out.println("End of Chapter 4 Challenges");
+		System.out.println();
+	}
+}
