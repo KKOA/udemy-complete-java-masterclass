eb32d2a2079de1738c9602b5b269df219db6a4c8
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-09 20:51:58 +0100

    Added MyMain Class to package ch04.exercises. Added references to all classes in same package.

---
+++ udemy-complete-java-masterclass/src/ch04/exercises/MyMain.java
@@ -0,0 +1,19 @@
+package ch04.exercises;
+
+public class MyMain {
+	public static void main() {
+		System.out.println("Chapter 4 Exercises");
+		System.out.println();
+
+		Variable.main();
+		BtyeShortInt.main();
+		FloatDouble.main();
+		BoolChar.main();
+		StringType.main();
+		Operator.main();
+
+		System.out.println();
+		System.out.println("End of Chapter 4 Exercises");
+		System.out.println();
+	}
+}
