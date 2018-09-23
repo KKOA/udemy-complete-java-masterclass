0b3ae57bca86500920a047f7f087fc8e3d8ce998
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-23 23:27:56 +0100

    Added Ch07Challenges to ch07.challenges package.

---
+++ udemy-complete-java-masterclass/src/ch07/challenges/Ch07Challenges.java
@@ -0,0 +1,17 @@
+package ch07.challenges;
+
+public class Ch07Challenges {
+
+	public static void main() {
+		System.out.println("Chapter 7 Challenges");
+		System.out.println();
+
+		AccountMain.main();
+		VipCustomerMain.main();
+
+		System.out.println();
+		System.out.println("End of Chapter 7 Challenges");
+		System.out.println();
+	}
+
+}
