cc7b3f9ea79c230b9e15350f892d69e8a28d5ce1
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-25 21:47:14 +0100

    Added Ch08Challenges class to ch08.challenges package.

---
+++ udemy-complete-java-masterclass/src/ch08/challenges/Ch08Challenges.java
@@ -0,0 +1,18 @@
+package ch08.challenges;
+
+public class Ch08Challenges {
+
+	public static void main() {
+		System.out.println("Chapter 8 Challenges");
+		System.out.println();
+
+		RoomMain.main();
+		PrinterMain.main();
+		CarMain.main();
+
+		System.out.println();
+		System.out.println("End of Chapter 8 Challenges");
+		System.out.println();
+	}
+
+}
