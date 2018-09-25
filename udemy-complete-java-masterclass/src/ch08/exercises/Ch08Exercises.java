c0cc0bf01c44e20c6bda12ade7fc25e80446fb66
keith(MacBookPro) <ka33145@googlemail.com>
2018-09-25 19:51:28 +0100

    Added Ch08Exercises to Ch08.exercises package.

---
+++ udemy-complete-java-masterclass/src/ch08/exercises/Ch08Exercises.java
@@ -0,0 +1,19 @@
+package ch08.exercises;
+
+public class Ch08Exercises {
+
+	public static void main() {
+		System.out.println("Chapter 8 Exercises");
+		System.out.println();
+
+		VehicleMain.main();
+		PCMain.main();
+		PlayerMain.main();
+		MovieMain.main();
+
+		System.out.println();
+		System.out.println("End of Chapter 8 Exercises");
+		System.out.println();
+	}
+
+}
