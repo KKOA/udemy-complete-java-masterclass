37c19eaa44c049a15b7d43e56f0341ee1955d987
keith(MacBookPro) <ka33145@googlemail.com>
2018-10-01 07:09:36 +0100

    Added Ch09Exercises and ch09Challenges to ch09 package.

---
+++ udemy-complete-java-masterclass/src/ch09/challenges/Ch09Challenges.java
@@ -0,0 +1,22 @@
+package ch09.challenges;
+
+import java.util.Scanner;
+
+public class Ch09Challenges {
+
+	public static void main(Scanner scanner) {
+
+		System.out.println("Chapter 9 Challenges");
+		System.out.println();
+
+//		SortArray.main(scanner);
+//		MobileMain.main();
+//
+//		BankMain.main();
+
+		System.out.println();
+		System.out.println("End of Chapter 9 Challenges");
+		System.out.println();
+
+	}
+}
---
+++ udemy-complete-java-masterclass/src/ch09/exercises/Ch09Exercises.java
@@ -0,0 +1,18 @@
+package ch09.exercises;
+
+import java.util.Scanner;
+
+public class Ch09Exercises {
+	public static void main(Scanner scanner) {
+		System.out.println("Chapter 9 Exercises");
+		System.out.println();
+
+//		MyArray.main(scanner);
+		ReferenceVsValueTypes.main();
+
+		System.out.println();
+		System.out.println("End of Chapter 9 Exercises");
+		System.out.println();
+	}
+
+}
